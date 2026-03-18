74fc9ee0ae3d3448949589c62af7f555afa38596
python_mini_project_11 <vishalrajputvr9592057@gamil.com>
2026-03-18 13:32:23 +0530

    add new file

---
+++ addtwoNumbare.py
@@ -0,0 +1,14 @@
+num1 = 30
+num2 = 40
+
+
+print("the sum of given two numbars", num1+num2)
+
+n1 = int(input("enter a  first  numbar :  "))
+n2 = int(input("enter a  second numbar  :  "))
+
+adition = n1 + n2
+
+print("the sum of given two numbars", adition)
+
+
